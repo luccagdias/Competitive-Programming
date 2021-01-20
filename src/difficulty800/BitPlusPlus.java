0087ffdca65970e08d0a09e816c53e17348c70e4
Lucca Dias <lucca.gdias07@gmail.com>
2021-01-20 20:28:46 -0300

    Challenge: Bit++

---
+++ src/difficulty800/BitPlusPlus.java
@@ -0,0 +1,24 @@
+package difficulty800;
+
+import java.util.Scanner;
+
+public class BitPlusPlus {
+    public static void main(String[] args) {
+        short result = 0;
+        Scanner scanner = new Scanner(System.in);
+        short numberOfStatements = scanner.nextShort();
+        scanner.nextLine();
+
+        for (int i = 0; i < numberOfStatements; i++) {
+            String statement = scanner.nextLine();
+
+            if (statement.contains("+")) {
+                result++;
+            } else {
+                result--;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
