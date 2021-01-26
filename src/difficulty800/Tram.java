4dfb8406c0c49b10766998ceb3f07797e928f025
Lucca Dias <lucca.gdias07@gmail.com>
2021-01-26 18:33:34 -0300

    Challenge: Tram

---
+++ src/difficulty800/Tram.java
@@ -0,0 +1,23 @@
+package difficulty800;
+
+import java.util.Scanner;
+
+public class Tram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStops = scanner.nextInt();
+        int totalPassengers = 0;
+        int minimumCapacity = 0;
+
+        for (int i = 0; i < numberOfStops; i++) {
+            totalPassengers -= scanner.nextInt();;
+            totalPassengers += scanner.nextInt();
+
+            if (totalPassengers > minimumCapacity) {
+                minimumCapacity = totalPassengers;
+            }
+        }
+
+        System.out.println(minimumCapacity);
+    }
+}
