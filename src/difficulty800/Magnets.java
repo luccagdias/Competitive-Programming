589d51e0987bc3d457cdb5d7ce91fe1bf606398b
Lucca Dias <lucca.gdias07@gmail.com>
2021-01-31 09:58:01 -0300

    Challenge: Magnets

---
+++ src/difficulty800/Magnets.java
@@ -0,0 +1,28 @@
+package difficulty800;
+
+import java.util.Scanner;
+
+public class Magnets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numOfMagnets = scanner.nextInt();
+        scanner.nextLine();
+        String lastMagnet = "22";
+        int groups = 1;
+
+        for (int i = 0; i < numOfMagnets; i++) {
+            String magnet = scanner.nextLine();
+
+            if (lastMagnet.charAt(1) == magnet.charAt(0)) {
+                groups ++;
+            }
+
+            lastMagnet = magnet;
+        }
+
+        if (groups == 0) {
+            groups++;
+        }
+        System.out.println(groups);
+    }
+}
