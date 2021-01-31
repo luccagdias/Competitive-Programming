d45caf31600557a0ed879a2bf508e3214eca6b00
Lucca Dias <lucca.gdias07@gmail.com>
2021-01-31 08:48:06 -0300

    Challenge: George and Accommodation

---
+++ src/difficulty800/GeorgeAndAccommodation.java
@@ -0,0 +1,22 @@
+package difficulty800;
+
+import java.util.Scanner;
+
+public class GeorgeAndAccommodation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        byte rooms = scanner.nextByte();
+        byte total = 0;
+
+        for (int i = 0; i < rooms; i++) {
+            byte people = scanner.nextByte();
+            byte capacity = scanner.nextByte();
+
+            if (capacity - people >= 2) {
+                total++;
+            }
+        }
+
+        System.out.println(total);
+    }
+}
