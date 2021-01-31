f03585378c20fde15d10a6477299cb1ed3218ff7
Lucca Dias <lucca.gdias07@gmail.com>
2021-01-31 09:03:12 -0300

    Challenge: Vanya and Fence

---
+++ src/difficulty800/VanyaAndFence.java
@@ -0,0 +1,19 @@
+package difficulty800;
+
+import java.util.Scanner;
+
+public class VanyaAndFence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        short numOfFriends = scanner.nextShort();
+        short heightOfFence = scanner.nextShort();
+        int sizeOfRoad = 0;
+
+        for (int i = 0; i < numOfFriends; i++) {
+            short heightOfFriend = scanner.nextShort();
+            sizeOfRoad = (heightOfFriend > heightOfFence) ? (sizeOfRoad += 2) : (sizeOfRoad + 1);
+        }
+
+        System.out.println(sizeOfRoad);
+    }
+}
