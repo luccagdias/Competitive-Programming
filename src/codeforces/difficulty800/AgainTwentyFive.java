2c69fd7bcaac1a94cfa7da349e15921b26cca78c
Lucca Dias <lucca.gdias07@gmail.com>
2021-02-23 22:07:50 -0300

    630A - Again Twenty Five!

---
+++ src/codeforces/difficulty800/AgainTwentyFive.java
@@ -0,0 +1,9 @@
+package codeforces.difficulty800;
+
+import java.util.Scanner;
+
+public class AgainTwentyFive {
+    public static void main(String[] args) {
+        System.out.println(25);
+    }
+}
